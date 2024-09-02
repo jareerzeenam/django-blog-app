83a2e7147a773f381f52147a40bfe33469df2284
Jareer Zeenam <jareerzeenam.28@gmail.com>
2024-09-03 00:34:05 +0100

    Show Posts list and view post completed

--- myProject/posts/views.py
+++ myProject/posts/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from .models import Post
+from django.http import HttpResponse
 
 # Create your views here.
 
 
 def posts_list(request):
-    return render(request, 'posts/posts_list.html')
+    posts = Post.objects.all().order_by('-date')
+    return render(request, 'posts/posts_list.html', {'posts': posts})
+
+def posts_page(request, slug):
+    # return HttpResponse(slug)
+    post = Post.objects.get(slug=slug)
+    return render(request, 'posts/posts_page.html', {'post': post})
