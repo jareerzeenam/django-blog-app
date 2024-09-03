17014e20cd9c6d9f0b57fd66ddee035e8fd333d8
Jareer Zeenam <jareerzeenam.28@gmail.com>
2024-09-03 02:21:25 +0100

    User Login feature added

--- myProject/users/views.py
+++ myProject/users/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login
 
 # Register view
 def register_view(request):
     # Save the form data if the form is valid
-    if request.method == 'POST':
+    if request.method == 'POST': 
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            login(request, form.save())
             return redirect('posts:list') 
     else:
         # UserCreationForm is a built-in form in Django
@@ -15,3 +16,15 @@ def register_view(request):
     
     # return the form to the template
     return render(request, 'users/register.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            #! Log the user in
+            login(request, form.get_user())
+            return redirect('posts:list') 
+    else:
+        form = AuthenticationForm()
+        
+    return render(request, 'users/login.html', {'form': form})
