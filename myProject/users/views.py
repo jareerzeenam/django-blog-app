66f3819125a73394f4cad34992cac40900e36772
Jareer Zeenam <jareerzeenam.28@gmail.com>
2024-09-03 02:01:27 +0100

    Register User completed

--- myProject/users/views.py
+++ myProject/users/views.py
@@ -1,5 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+# Register view
 def register_view(request):
-    return render(request, 'users/register.html')
+    # Save the form data if the form is valid
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('posts:list') 
+    else:
+        # UserCreationForm is a built-in form in Django
+        form = UserCreationForm()
+    
+    # return the form to the template
+    return render(request, 'users/register.html', {'form': form})
