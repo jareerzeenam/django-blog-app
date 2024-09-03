c77060a08dd1658fc6b4ce7dee40175b073cf046
Jareer Zeenam <jareerzeenam.28@gmail.com>
2024-09-03 18:36:38 +0100

    Create post feature added

--- myProject/posts/views.py
+++ myProject/posts/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Post
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from . import forms
 
 # Create your views here.
 
@@ -17,4 +18,15 @@ def posts_page(request, slug):
 
 @login_required(login_url='/users/login/')
 def post_new(request):
-    return render(request, 'posts/posts_new.html')
+    if request.method == 'POST':
+        form = forms.CreatePost(request.POST, request.FILES)
+        if form.is_valid():
+            # save with user
+            newPost = form.save(commit=False)
+            newPost.author = request.user
+            newPost.save()
+            return redirect('posts:list')
+    else : 
+        form = forms.CreatePost()
+        
+    return render(request, 'posts/posts_new.html', {'form': form})
